c4cb52e0867c671b0bea7e31b2f4bcfb25665ba2
Akshit <akshitkumar540@gmail.com>
2021-09-21 07:19:39 +0530

    Show page done

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,58 +1,93 @@
-import React,{useEffect,useState} from 'react';
-import {useParams} from 'react-router-dom'
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, show : action.show, error: null };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading : false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: true,
+};
 const Show = () => {
+  const { id } = useParams();
 
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-const {id} = useParams();
-const [show, setShow] = useState(null);
-const [isLoading,setIsLoading]=useState(true);
-const [error, setError] = useState(null);
-    
-useEffect(() => {
+  useEffect(() => {
+    let isMounted = true;
 
-    let isMounted= true;
-    
-    apiGet(`/shows/${id}?embeded[]=seasons&embed[]=cast`).then(results=>{
-     
-            if(isMounted){
-            setShow(results);
-setIsLoading(false);
-console.log(results);}
-            
-       
-
-
-    }).catch(err=>{
-        if(isMounted){
-        setError(err.message);
-        setIsLoading(false);
-        console.log(show);
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+          console.log(results);
+        if (isMounted) {
+          
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
-    });
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
 
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
 
-return ()=>{
+  if (isLoading) {
+    return <div>The Data is Loading</div>;
+  }
+  if (error) {
+    return <div>Error occured </div>;
+  }
 
-    isMounted=false;
-}
+  return (
+    <ShowPageWrapper>
+      
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details status={show.status} network={show.network} premiered={show.premiered}/>
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
+};
 
-
-}, [id])
-
-if(isLoading){
-    return <div>The Data is Loading</div>
-}
-if(error){
-    return <div>Error occured </div>
-}
-
-    return (
-        <div>
-            this is show page
-        </div>
-    )
-}
-
-export default Show
+export default Show;
