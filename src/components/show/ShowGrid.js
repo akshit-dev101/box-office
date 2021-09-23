9cf870be1285ee6ed039630f5a0de8f17402795d
Akshit <akshitkumar540@gmail.com>
2021-09-23 08:44:31 +0530

    custom hooks

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React from 'react';
 import ShowCard from './ShowCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
-    return (
-        <FlexGrid>
-            {
+
+const [starredShows,dispatchStarred]=useShows()
 
 
-                data.map(({ show }) => <ShowCard key={show.id} id={show.id} name={show.name} image={show.image ?
-                    show.image.medium : IMAGE_NOT_FOUND} summary={show.summary} />)}
+
+  return (
+    <FlexGrid>
+      {data.map(({ show }) => {
+
+        const isStarred=starredShows.includes(show.id);
+        const onStarClick=()=>{
+            if(isStarred){
+                dispatchStarred({type:'REMOVE',showId:show.id})
+            } else {
+
+                dispatchStarred({type:'ADD',showId: show.id})
+            }
 
 
-        </FlexGrid>
-    )
-}
+        }
 
-export default ShowGrid
+        return(<ShowCard
+          key={show.id}
+          id={show.id}
+          name={show.name}
+          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+          summary={show.summary}
+          onStarClick={onStarClick}
+          isStarred={isStarred}
+        />)
+      })}
+    </FlexGrid>
+  );
+};
+
+export default ShowGrid;
